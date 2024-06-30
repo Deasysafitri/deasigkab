393f1dfd35a4684f3443ae5217eb5085df55f125
Deasysafitri <deasysafitrii@gmail.com>
2024-06-30 17:56:47 +0700

    deasigkab

---
+++ websig.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2024 at 04:12 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `websig`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kabupaten`
+--
+
+CREATE TABLE `kabupaten` (
+  `id_kabupaten` int(11) NOT NULL,
+  `kode_kabupaten` varchar(100) NOT NULL,
+  `nama_kabupaten` varchar(100) NOT NULL,
+  `koordinat` varchar(100) NOT NULL,
+  `jumlah_penduduk` int(11) NOT NULL,
+  `luas_wilayah` float(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kabupaten`
+--
+
+INSERT INTO `kabupaten` (`id_kabupaten`, `kode_kabupaten`, `nama_kabupaten`, `koordinat`, `jumlah_penduduk`, `luas_wilayah`) VALUES
+(1, '3302', 'Banyumas', '-7.362274826893668, 109.10943969842893', 82317924, 17083020.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kecamatan`
+--
+
+CREATE TABLE `kecamatan` (
+  `kode_kecamatan` varchar(100) NOT NULL,
+  `id_kabupaten` int(11) NOT NULL,
+  `nama_kecamatan` varchar(100) NOT NULL,
+  `jumlah_penduduk` int(11) NOT NULL,
+  `luas_wilayah` float(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kecamatan`
+--
+
+INSERT INTO `kecamatan` (`kode_kecamatan`, `id_kabupaten`, `nama_kecamatan`, `jumlah_penduduk`, `luas_wilayah`) VALUES
+('3302010', 1, 'LUMBIR', 3456, 34.00),
+('3302020', 1, 'Wangon', 1234, 23.00),
+('3302030', 1, 'jatilawang', 4567, 1234.00),
+('3302040', 1, 'rawalo', 1234, 54.00),
+('3302050', 1, 'Kebasen', 12436, 36387.00),
+('3302130', 1, 'purwojati', 3456, 22.00),
+('3302140', 1, 'Ajibarang', 3456, 42211.00),
+('3302160', 1, 'pekuncen', 12345678, 44.00),
+('3302740', 1, 'Purwokerto Utara', 48264, 90139.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sekolah`
+--
+
+CREATE TABLE `sekolah` (
+  `npsn` int(11) NOT NULL,
+  `kode_kecamatan` varchar(100) NOT NULL,
+  `nama_sekolah` varchar(100) NOT NULL,
+  `alamat_sekolah` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `jenjang_pendidikan` varchar(100) NOT NULL,
+  `koordinat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sekolah`
+--
+
+INSERT INTO `sekolah` (`npsn`, `kode_kecamatan`, `nama_sekolah`, `alamat_sekolah`, `status`, `jenjang_pendidikan`, `koordinat`) VALUES
+(20302727, '3302140', 'SD N 1 PANDAK', 'Jl.raya Baturraden Timur  No.38', 'NEGERI', 'SD', '-7.3767383,109.2405817');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kabupaten`
+--
+ALTER TABLE `kabupaten`
+  ADD PRIMARY KEY (`id_kabupaten`);
+
+--
+-- Indexes for table `kecamatan`
+--
+ALTER TABLE `kecamatan`
+  ADD PRIMARY KEY (`kode_kecamatan`),
+  ADD KEY `kecamatan_ibfk_1` (`id_kabupaten`);
+
+--
+-- Indexes for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  ADD PRIMARY KEY (`npsn`),
+  ADD UNIQUE KEY `kode_kecamatan` (`kode_kecamatan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kabupaten`
+--
+ALTER TABLE `kabupaten`
+  MODIFY `id_kabupaten` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kecamatan`
+--
+ALTER TABLE `kecamatan`
+  ADD CONSTRAINT `kecamatan_ibfk_2` FOREIGN KEY (`id_kabupaten`) REFERENCES `kabupaten` (`id_kabupaten`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
